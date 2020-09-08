e1bcacec286e828af49f71caa880922ad3405878
Giuseppe1992 <giuseppedilena92@gmail.com>
2020-09-08 22:53:53 +0200

    cookie consent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
+import CookieConsent, { Cookies } from "react-cookie-consent";
 
 function App() {
   return (
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -17,6 +17,7 @@ import Twitter from "./twitter-embed/twitter";
 import {StyleProvider} from "../contexts/StyleContext";
 import "./Main.css";
 import Profile from "./profile/Profile";
+import CookieConsent from "react-cookie-consent";
 
 export default class Main extends Component {
   constructor(props) {
@@ -45,6 +46,16 @@ export default class Main extends Component {
         <StyleProvider
           value={{isDark: this.state.isDark, changeTheme: this.changeTheme}}
         >
+          <CookieConsent
+            location="bottom"
+            buttonText="Sure man!!"
+            cookieName="myAwesomeCookieName2"
+            style={{ background: "#2B373B" }}
+            buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+            expires={150}
+          >
+            This website uses cookies to enhance the user experience.{" "}
+          </CookieConsent>
           <Header />
           <Greeting />
           <Skills />
