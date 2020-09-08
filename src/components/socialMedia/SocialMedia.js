dd9264aa187c309af6caf7a83f26034433df4c7a
Giuseppe1992 <giuseppedilena92@gmail.com>
2020-09-08 10:25:17 +0200

    Mail ico and positions

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -5,28 +5,28 @@ import { socialMediaLinks } from "../../portfolio";
 export default function socialMedia() {
   return (
     <div className="social-media-div">
+
+      {socialMediaLinks.gmail ?
+          <a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="_blank">
+            <i className="fas fa-envelope"></i>
+            <span></span>
+          </a>
+          :
+          null}
+
+      {socialMediaLinks.linkedin ?
+          <a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank">
+            <i className="fab fa-linkedin-in"></i>
+            <span></span>
+          </a>
+          :
+          null}
       
       {socialMediaLinks.github ? 
       (<a href={socialMediaLinks.github} className="icon-button github" target="_blank">
         <i className="fab fa-github"></i>
         <span></span>
       </a>)
-      :
-      null}
-
-      {socialMediaLinks.linkedin ?
-      <a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank">
-        <i className="fab fa-linkedin-in"></i>
-        <span></span>
-      </a>
-      :
-      null}
-
-      {socialMediaLinks.gmail ?
-      <a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="_blank">
-        <i className="fab fa-google"></i>
-        <span></span>
-      </a>
       :
       null}
 
