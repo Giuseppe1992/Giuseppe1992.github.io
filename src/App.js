3e5d3493875272c9362502d0c2647040b05b3a73
Giuseppe1992 <giuseppedilena92@gmail.com>
2020-09-08 22:53:53 +0200

    cookie consent

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./containers/Main";
+import CookieConsent, { Cookies } from "react-cookie-consent";
 
 function App() {
   return (
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -15,11 +15,22 @@ import Podcast from "./podcast/Podcast";
 import Top from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 import Profile from "./profile/Profile";
+import CookieConsent from "react-cookie-consent";
 
 export default class Main extends Component {
   render() {
     return (
       <div>
+          <CookieConsent
+              location="bottom"
+              buttonText="Sure man!!"
+              cookieName="myAwesomeCookieName2"
+              style={{ background: "#2B373B" }}
+              buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+              expires={150}
+          >
+              This website uses cookies to enhance the user experience.{" "}
+          </CookieConsent>
         <Header />
         <Greeting />
         <Skills />
